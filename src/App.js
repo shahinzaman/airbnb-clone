3261e460ec0c98112478e32fb7b7110969451b41
Shahin Zaman <63395910+shahinzaman@users.noreply.github.com>
2024-10-19 17:03:35 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
 import './App.css';
 import Home from './Home';
-import Header from './Header'
-import Footer from './Footer'
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import SearchPage from './SearchPage'
+import Header from './Header';
+import Footer from './Footer';
+import SearchPage from './SearchPage';
 import Toggle from "./Toggle";
 import { useContext } from 'react';
 import { ThemeContext } from './context';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const theme = useContext(ThemeContext);
   const darkMode = theme.state.darkMode;
 
   return (
-
-    // BEM
-    <div className="App" style={{
-      backgroundColor: darkMode ? "#222" : "white",
-      color: darkMode && "white",
-    }}>
+    <div
+      className="App"
+      style={{
+        backgroundColor: darkMode ? "#222" : "white",
+        color: darkMode ? "white" : "black",
+      }}
+    >
       <Router>
         <Header />
         <Toggle />
-        <Switch>
-          <Route path="/search">
-            <SearchPage />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
         <Footer />
-      </ Router>
-    </div >
+      </Router>
+    </div>
   );
 }
 
